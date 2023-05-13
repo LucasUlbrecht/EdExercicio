9c9b31c9d0d916d82d42c349eca2782cbdfd40e1
Lucas Ulbrecht <luke.patrizi.07@gmail.com>
2023-05-13 17:34:01 -0400

    atualizacao basica interface

--- ex.h
+++ ex.h
@@ -1,12 +1,37 @@
 #ifndef _EDTRABAIO_
 #define _EDTRABAIO_
-
+#define MAXNOMEMUNICIPIO 29;
+#define MAXSIGLAUF 2;
+#define MAXNOMEESTADO 17;
+#define MAXNOMECAPITAL 8;
+#define MAXNOMEREGIAO 9;
 typedef struct _node{
-    void * item;
+    void *informacao;
+    struct _node *pai;
     struct _node *esq;
     struct _node *dir;
     int h;
 }tnode;
+
+typedef struct MunicipiosBrasileiros
+{
+    int Cod
+    char[MAXNOMEMUNICIPIO+1] NomeMuni;
+    int	CodUF
+    char[MAXSIGLAUF+1] SiglaUF;
+    char[MAXNOMEESTADO+1]	Estado;
+    char[MAXNOMECAPITAL+1]	Capital;
+    int	Latitude;
+    int Longitude;
+    char[MAXNOMEREGIAO+1] Região;
+    int CodSiafi;
+    int	DDD;
+    int FusoHorario;
+    kdT* cabeca;
+    kdT* fEsq;
+    kdT* fDir;
+}MunicBr;
+
 
 
 void avl_insere(tnode ** parv,titem reg);
