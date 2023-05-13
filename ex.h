9692d8a01c1b69b7eb435f862e785f4d473b01dc
Lucas Ulbrecht <luke.patrizi.07@gmail.com>
2023-05-13 17:32:55 -0400

    atualizacao basica interface

--- ex.h
+++ ex.h
@@ -1,25 +1,15 @@
 #ifndef _EDTRABAIO_
-#def _EDTRABAIO_
-#define MAXNOMEMUNICIPIO 29;
-#define MAXSIGLAUF 2;
-#define MAXNOMEESTADO 17;
-#define MAXNOMECAPITAL 8;
-#define MAXNOMEREGIAO 9;
-typedef struct kdTree{
-    int Cod
-    char[MAXNOMEMUNICIPIO+1] NomeMuni;
-    int	CodUF
-    char[MAXSIGLAUF+1] SiglaUF;
-    char[MAXNOMEESTADO+1]	Estado;
-    char[MAXNOMECAPITAL+1]	Capital;
-    int	Latitude;
-    int Longitude;
-    char[MAXNOMEREGIAO+1] Região;
-    int CodSiafi;
-    int	DDD;
-    int FusoHorario;
-    kdT* cabeca;
-    kdT* fEsq;
-    kdT* fDir;
-}kdT;
+#define _EDTRABAIO_
+
+typedef struct _node{
+    void * item;
+    struct _node *esq;
+    struct _node *dir;
+    int h;
+}tnode;
+
+
+void avl_insere(tnode ** parv,titem reg);
+void avl_rebalancear(tnode **parv);
+
 #endif
